541acd02e2578f51e960aee7f38338c42159189c
Nikolas Edric Santoso <gameknight999credric@gmail.com>
2026-03-29 15:16:09 -0400

    cors()

--- backend/index.js
+++ backend/index.js
@@ -11,9 +11,10 @@ const app = express();
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
 // TODO (Edited): complete me (CORS)
-app.use(cors({
-    origin: FRONTEND_URL
-}));
+// app.use(cors({
+//     origin: FRONTEND_URL
+// }));
+app.use(cors());
 app.use(express.json());
 app.use('/', routes);
 
