32418f6bf68e4396218ccc5930e3ee961c2eeb6e
Nikolas Edric Santoso <gameknight999credric@gmail.com>
2026-03-29 15:29:37 -0400

    added CORS logic

--- backend/index.js
+++ backend/index.js
@@ -11,12 +11,10 @@ const app = express();
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
 // TODO (Edited): complete me (CORS)
-// app.use(cors({
-//     origin: FRONTEND_URL
-// }));
-app.use(cors());
+app.use(cors({
+    origin: FRONTEND_URL
+}));
 app.use(express.json());
 app.use('/', routes);
-app.options('*', cors());
 
 export default app;
